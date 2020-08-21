3034d2c5f3dec547868a09f5695a10e98222f9b3
Flex Zhong <chungzh07@gmail.com>
2020-08-21 13:33:27 +0800

    Update preferenceswindowbackend.hpp

--- src/preferenceswindowbackend.hpp
+++ src/preferenceswindowbackend.hpp
@@ -8,24 +8,23 @@
 class PreferencesWindowBackend : public QObject
 {
     Q_OBJECT
-    Q_PROPERTY(QStringList shThemeList READ shThemeList)
-    Q_PROPERTY(int currentIndex READ currentIndex)
-    Q_PROPERTY(QString currentName WRITE setCurrentName)
+    Q_PROPERTY(QStringList shThemeList MEMBER m_shThemeList)
+    Q_PROPERTY(int currentIndex READ currentIndex NOTIFY currentIndexChanged)
+    Q_PROPERTY(QString currentName MEMBER m_currentName)
 
   public:
     PreferencesWindowBackend()
     {
-    }
-
-    QStringList shThemeList() const
-    {
-        return Npanda::common::shThemeList;
+        m_shThemeList = Npanda::common::shThemeList;
     }
 
     int currentIndex() const
     {
         return Npanda::common::shThemeList.indexOf(m_currentName);
     }
+
+  signals:
+    void currentIndexChanged(int curIdx);
 
   public slots:
     void setCurrentName(const QString &name)
@@ -35,6 +34,7 @@ class PreferencesWindowBackend : public QObject
 
   private:
     QString m_currentName;
+    QStringList m_shThemeList;
 };
 
 #endif // PREFERENCESWINDOWBACKEND_HPP
