28f4e31b1f247b9a5e65cf6a29068ba7307cdef4
Flex Zhong <chungzh07@gmail.com>
2020-08-21 10:12:49 +0800

    fix

---
+++ src/aboutwindowbackend.hpp
@@ -0,0 +1,43 @@
+#ifndef ABOUTWINDOWBACKEND_HPP
+#define ABOUTWINDOWBACKEND_HPP
+
+#include <QDebug>
+#include <QFile>
+#include <QObject>
+#include <QUrl>
+
+class AboutWindowBackend : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QString creditText READ creditText WRITE setCreditText NOTIFY creditTextChanged)
+
+  public:
+    AboutWindowBackend()
+    {
+        setCreditText(":/assets/CREDIT.md");
+    }
+
+    QString creditText()
+    {
+        return m_creditText;
+    }
+
+  signals:
+    void creditTextChanged();
+
+  public slots:
+    void setCreditText(const QString &creditTextSource)
+    {
+        QFile file(creditTextSource);
+        if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+            return;
+        m_creditText = file.readAll();
+        emit creditTextChanged();
+        file.close();
+    }
+
+  private:
+    QString m_creditText;
+};
+
+#endif // ABOUTWINDOWBACKEND_HPP
